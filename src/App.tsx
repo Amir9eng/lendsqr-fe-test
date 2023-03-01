0a174f08bd1b91be5786c79ac10f66e21734225f
admin <admin@admins-MacBook-Pro.local>
2023-03-01 22:15:52 +0100

    setting up pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import DashboardLayout from './layout/Dashboard';
+import Login from './pages/Login';
+import Users from './pages/users/Users';
+import Dashboard from './layout/Dashboard';
+import UserDetails from './pages/users/UserDetails';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route index path="users" element={<Users />} />
+        <Route path="users/:id" element={<UserDetails />} />
+        <Route path="*" element={<h2>'Page Not found'</h2>} />
+      </Routes>
     </div>
   );
 }
